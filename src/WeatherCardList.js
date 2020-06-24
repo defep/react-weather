57d33b069f2d06987239295ebcc7661046ea1cc2
defep <federico.palma@hotmail.com>
2020-06-24 17:07:57 -0300

    process and show Open Weather API results

--- src/WeatherCardList.js
+++ src/WeatherCardList.js
@@ -5,25 +5,72 @@ import './WeatherCardList.css'
 
 class WeatherCardList extends Component {
 
+    weekdays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+
     constructor() {
         super()
         this.state = {
             scale: "C",
-            weatherCards: [
-                { day: 'Mon', minTemp: this.getRandomMinTemp(), maxTemp: this.getRandomMaxTemp() },
-                { day: 'Tue', minTemp: this.getRandomMinTemp(), maxTemp: this.getRandomMaxTemp() },
-                { day: 'Wed', minTemp: this.getRandomMinTemp(), maxTemp: this.getRandomMaxTemp() },
-                { day: 'Thu', minTemp: this.getRandomMinTemp(), maxTemp: this.getRandomMaxTemp() },
-                { day: 'Fri', minTemp: this.getRandomMinTemp(), maxTemp: this.getRandomMaxTemp() },
-            ],
+            weatherCards: [],
             active: 0
         }
     }
 
     componentDidMount() {
-        Axios.get('https://api.openweathermap.org/data/2.5/forecast?lat=-34.603722&lon=-58.381592&appid=' + process.env.REACT_APP_API_KEY).then(res => {
-            console.log(res.data);
-        });
+        Axios.get('https://api.openweathermap.org/data/2.5/forecast?lat=-34.603722&lon=-58.381592&appid=' + process.env.REACT_APP_API_KEY)
+            .then(res => {
+                let cards = [];
+                let last_weekday = '';
+                let day_min_temp = [];
+                let day_max_temp = [];
+                let last_item;
+                res.data.list.forEach((item,i) => {
+                    let datetime = new Date(item.dt*1000);
+                    let weekday = datetime.getDay();
+
+                    if (last_weekday === '') {
+                        last_weekday = weekday;
+                        last_item = item;
+                    }
+                    
+                    if (weekday !== last_weekday || (res.data.list.length - 1) === i) {
+
+                        if ((res.data.list.length - 1) === i) {
+                            day_min_temp.push(item.main.temp_min);
+                            day_max_temp.push(item.main.temp_max);
+                        }
+                        
+                        last_item['temp_min'] = Math.round(Math.min(...day_min_temp) - 273.15);
+                        last_item['temp_max'] = Math.round(Math.max(...day_max_temp) - 273.15);
+
+                        cards.push(last_item);
+                        day_min_temp = [];
+                        day_max_temp = [];
+                    }                   
+
+                    day_min_temp.push(item.main.temp_min);
+                    day_max_temp.push(item.main.temp_max);
+
+                    last_weekday = weekday;
+                    last_item = item;
+                });
+
+                cards = cards.slice(1, 6);
+
+                cards.forEach(card => {
+                    let date = new Date(card.dt * 1000);
+                    card['day'] = this.weekdays[date.getDay()];
+                    card['minTemp'] = card.temp_min;
+                    card['maxTemp'] = card.temp_max;
+                })
+
+                this.setState({
+                    weatherCards: cards
+                })
+            })
+            .catch(function (e) {
+                console.log("Error ", e);
+            });
     }
 
     getRandomMinTemp() {
@@ -67,13 +114,8 @@ class WeatherCardList extends Component {
         this.setState({ scale: scale, weatherCards: weatherCards });
     }
 
-    createWeatherCard() {
-        return <WeatherCard day="OK" />
-    }
-
     render() {
-
-        const weatherCards = this.state.weatherCards.map((item, index) => {
+        let weatherCards = this.state.weatherCards.map((item, index) => {
             return <WeatherCard key={index}
                 day={item.day}
                 minTemp={item.minTemp}
